a24eb1646cd57bb38771a8ab73d5fe832ae68ea7
Fannsan <112868096+Fannsan@users.noreply.github.com>
2022-09-22 20:14:45 +0200

    Ny uppdatering
    Jag har snyggat till sidan och ändrat texten på vissa ställen

--- namn.js
+++ namn.js
@@ -1,12 +1,12 @@
 
 
 
-// jag skapar en i en array
+// jag skapar en constantvariabel som har en array men mina olika tebilder
 const names = ["tea1.png", "tea2.png", "matchate.jpg" , "fruktte.png", "puerh.jpg", "matt.jpg"];
 
-//Loopen, 
+//Här skapar jag loopen som ska plussas på med en och gå igenom min array
 for(let i = 0; i < names.length; i++){
-  //Här skapar jag en bild-tag
+  //Här skapar jag en bild-tag med namnet bild
   const bild = document.createElement("img");
   //här går jag igenom arrayen med bilder
   bild.src = names[i];
--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
+//Här skapar jag en variabel infoTe och tilldelar det mitt hämtade id omTe som finns i min HTML.
 var infoTe = document.getElementById('omTe');
 
+//Här gör jag en funktion som innebär att om man klickar på bilden så anropas klassen "mer info" och visar den andra bilden.
 infoTe.addEventListener('click', function(infoTe){
     infoTe.target.classList.toggle('merInfo');
 })
 
 
-
+//skapar en constant som heter join och hämtar ID knapp och tilldelar join det värdet.
 const join = document.getElementById('knapp');
 
+//Här skapas en funktion som lissnar på att man trycker på knappen
 join.addEventListener('click', function(){
+    //skapar en let variabel för att den ska kunna ändras
     let text;
-    //skapar en variabel namnet 
+    //skapar en variabel namnet och hämtar det värdet som personen skrivit in i rutan
     const namnet = document.getElementById('name').value;
     //om det är en tom sträng så vill vi skicka ut att personen inte skrev in något
     if("" == (namnet)){
